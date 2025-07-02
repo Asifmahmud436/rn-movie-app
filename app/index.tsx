5592f7e87a8d4eca7e39dc38d0a7a48ea7643e23
Zettex <safaandsafa4@gmail.com>
2025-07-02 15:17:21 +0600

    initialized and added native wind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import "../global.css"
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center ">
+      <Text className="font-bold text-light-200 text-5xl">
+        Assalamu Alaikum!
+      </Text>
+    </View>
+  );
+}
